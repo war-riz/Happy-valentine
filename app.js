a5b8277dbec334580f5311dd28737ef2f8b608e0
war-riz <kehindewaris54@gmail.com>
2024-02-14 12:35:30 +0100

    Commit initial files

---
+++ app.js
@@ -0,0 +1,10 @@
+const btnFirst = document.getElementById('btn-first');
+const btnSecond = document.getElementById('btn-second');
+
+btnFirst.addEventListener('click', () => {
+    window.location = 'gift.html'
+})
+
+btnSecond.addEventListener('click', () => {
+    window.location = "don't.html"
+})
